41081473e1206e24fd2a43691373aead630e0bc2
unknown <johnkarlo.bar@gmail.com>
2023-02-07 19:40:58 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import data from './data.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  listHover = (userId) => {
+    const findEmployee = data.employees.filter(empInfo => empInfo.userId === userId);
+    const empInfo = findEmployee[0];
+
+    alert("Employee Info \nName: " + empInfo.preferredFullName + "\nJob Title Name: " + empInfo.jobTitleName + "\nEmployee Code: " + empInfo.employeeCode + "\nRegion: " + empInfo.region + "\nPhone number: " + empInfo.phoneNumber + "\nEmail Address: " + empInfo.emailAddress + "\nAge: " + empInfo.age + "\nDate of Birth: " + empInfo.dateOfBirth + "\nSalary: " + empInfo.salary + "\nDepartment: " + empInfo.department);
+  }
+
+  listData = data.employees.map(employeeInfo => {
+    return (
+      <ul key={employeeInfo.userId} onMouseOver={() => {this.listHover(employeeInfo.userId);}} style={{backgroundColor:"#b5e7a0",marginLeft:"auto",marginRight:"auto",width:"20%",padding:"10px",border:"0px solid black",borderRadius:"5px",fontFamily:"Cascadia Code"}}>
+        <li style={{ listStyleType: "none" }}><img src={employeeInfo.image} style={{width:"80%"}} /></li>
+        <li style={{ listStyleType: "none" }}>Name: {employeeInfo.preferredFullName}</li>
+        <li style={{ listStyleType: "none" }}>Designation: {employeeInfo.jobTitleName}</li>
+      </ul>
+    )
+  });
+
+  render() {
+    return (
+      <div style={{textAlign:"center"}}>
+        <p style={{fontSize:"30px",fontFamily:"Segoe UI Black",paddingBottom:"5px",borderBottom:"2px solid #878f99"}}>Employees</p>
+        {this.listData}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    "employees": [
+        {
+            "userId": "rirani",
+            "jobTitleName": "Director",
+            "firstName": "Romina",
+            "lastName": "Irani",
+            "preferredFullName": "Romin Irani",
+            "employeeCode": "E1",
+            "region": "CA",
+            "phoneNumber": "408-1234567",
+            "emailAddress": "romin.k.irani@gmail.com",
+            "age": 32,
+            "dateOfBirth": "11/01/1990",
+            "salary": "20000",
+            "image": "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "department": "HR"
+        },
+        {
+            "userId": "nirani",
+            "jobTitleName": "Developer",
+            "firstName": "Neil",
+            "lastName": "Irani",
+            "preferredFullName": "Neil Irani",
+            "employeeCode": "E2",
+            "region": "CA",
+            "phoneNumber": "408-1111111",
+            "emailAddress": "neilrirani@gmail.com",
+            "age": 28,
+            "dateOfBirth": "02/10/1995",
+            "salary": "20000",
+            "image": "https://images.pexels.com/photos/9301866/pexels-photo-9301866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "department": "IT"
+        },
+        {
+            "userId": "thanks",
+            "jobTitleName": "Developer",
+            "firstName": "Tom",
+            "lastName": "Hanks",
+            "preferredFullName": "Tom Hanks",
+            "employeeCode": "E3",
+            "region": "CA",
+            "phoneNumber": "408-2222222",
+            "emailAddress": "tomhanks@gmail.com",
+            "age": 30,
+            "dateOfBirth": "08/05/1992",
+            "salary": "20000",
+            "image": "https://images.pexels.com/photos/7845294/pexels-photo-7845294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "department": "IT"
+        }
+    ]
+}
+
+export default data;
